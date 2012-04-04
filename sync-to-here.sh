c801359625afed5619da530024f0e615174d609d
z7z8th <ybzhao1989@gmail.com>
2012-04-04 13:35:08 +0800

    add .bashrc

--- sync-to-here.sh
+++ sync-to-here.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 
 cd `pwd`/`dirname $0`
+echo -n "current dir: "
 pwd
 
-file_list='./.config/openbox
-./.config/openbox/lxde-rc.xml
-./.config/Trolltech.conf
-./.config/pcmanfm
-./.config/pcmanfm/LXDE.conf
-./.config/lxterminal
-./.config/lxterminal/lxterminal.conf
-./.config/lxpanel
-./.config/lxpanel/LXDE
-./.config/lxpanel/LXDE/config
-./.config/lxpanel/LXDE/panels
-./.config/lxpanel/LXDE/panels/panel'
+file_list='.bashrc
+.config/openbox
+.config/openbox/lxde-rc.xml
+.config/Trolltech.conf
+.config/pcmanfm
+.config/pcmanfm/LXDE.conf
+.config/lxterminal
+.config/lxterminal/lxterminal.conf
+.config/lxpanel
+.config/lxpanel/LXDE
+.config/lxpanel/LXDE/config
+.config/lxpanel/LXDE/panels
+.config/lxpanel/LXDE/panels/panel'
 
 echo "$file_list" | while read line;
 do
