3785ae756c81b3a5a4bff389abce5b69098488e9
z7z8th-rd <ybzhao1989@gmail.com>
2013-01-03 20:35:47 +0800

    add battery status to lxde panel. remove .orig from .gitignore don't diff identical file.

--- diff-everyfile.sh
+++ diff-everyfile.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 find usr home etc -type f | while read line;
 do
-    gvim -d /$line $line
+    remote_file=$line
+    local_file=/$line
+    diff $local_file $remote_file && continue
+    gvim -d  $local_file $remote_file
     while [ -n "$(ps aux|grep -v 'grep.*gvim'|grep gvim)" ]; do
         sleep 1s
     done
