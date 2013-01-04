2c8ad36efc205c8d06eb44c60a8491422de2b985
Bob.Zhao <bob.zhao@infomax.com.tw>
2013-01-04 15:35:16 +0800

    diff-everyfile.sh: use suffix to determine machine. use left scrollbar in gnome terminal. rename come configs with mach suffix. 'googleproxy www.google.com' like item didn't work in hosts, must be 'ip hostname' style, so change back.

--- diff-everyfile.sh
+++ diff-everyfile.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
-find usr home etc -type f | while read line;
-do
+
+hostlist='red im'
+for h in `echo $hostlist`; do
+    hostname | grep -iq $h && hosttype=$h
+done
+h=
+
+find etc -type f | 
+while read line; do
     remote_file=$line
     local_file=/$line
-    diff $local_file $remote_file && continue
-    gvim -d  $local_file $remote_file
-    while [ -n "$(ps aux|grep -v 'grep.*gvim'|grep gvim)" ]; do
-        sleep 1s
+
+    matchedhost=
+    for h in `echo $hostlist`; do
+        echo $line | grep -q "${h}\$" && { matchedhost=$h; break; }
     done
+    [ x$matchedhost != x -a x$matchedhost != x$hosttype ] && continue
+    [ x$matchedhost = x$hosttype ] && local_file=${local_file%.${hosttype}}
+    diff $local_file $remote_file >/dev/null 2>&1 && continue
+    gvim -f -d  $local_file $remote_file
+    # while [ -n "`ps aux|grep -v 'grep.*gvim'|grep gvim`" ]; do
+    #     sleep 1s
+    # done
 done
