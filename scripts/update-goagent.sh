88d543a3b1d46c3d5d22b0cf7cec0ed8ebe034b6
Bob.Zhao <bob.zhao@infomax.com.tw>
2014-01-14 09:52:28 +0800

    uniform ref in update-goagent.sh

--- scripts/update-goagent.sh
+++ scripts/update-goagent.sh
@@ -16,33 +16,36 @@ GA_LOCAL_CERTS=$GA_DIR/../local-certs.tar.bz2
 GA_EXEC=$GA_DIR_LOC/proxy.py
 GA_CONF=$GA_DIR_LOC/proxy.ini
 
+GA_HEAD=upstream/3.0
+
 cd $GA_DIR/
 appid=`awk '/appid/{print $3; exit;}' $GA_CONF`
 passwd=`awk '/password/ {print $3; exit;}' $GA_CONF`
 tar jcf $GA_LOCAL_CERTS local/certs/ local/CA.*
 git branch -a
-git reset --hard HEAD
+git reset --hard $GA_HEAD
 echo "=== do git clean -df"
 git clean -df >/dev/null
 # git checkout -b 3.0 remotes/upstream/3.0
 echo "=== do git pull"
 git remote update
-git checkout remotes/upstream/3.0
+git checkout $GA_HEAD
 echo "=== restore certs"
 rm -rf $GA_DIR_LOC/certs
 rm $GA_DIR_LOC/CA.*
 tar xf $GA_LOCAL_CERTS
 #cp ../CA.* $GA_DIR_LOC
 
-echo "======================================"
-echo "== backup->update->restore finished =="
-echo "======================================"
-
-
 echo "=== change proxy config"
 # read -p "input appid: " appid
 # read -p "input passwd: " passwd
 sed -i -r -e "s/^(appid\\s*=).*$/\\1 $appid/" -e "s/^(password\\s*=).*$/\\1 $passwd/" $GA_CONF
+
+
+echo "======================================"
+echo "== backup->update->restore finished =="
+echo "======================================"
+
 
 echo ""
 read -p "****** start goagent ?? *****" pp
